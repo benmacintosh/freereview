bec2eb06a8d6286159745c5b4ee086b845d87543
benmacintosh <ben.macintosh0@gmail.com>
2020-06-27 19:06:07 +1000

    update

--- static/mapcode.js
+++ static/mapcode.js
@@ -204,6 +204,7 @@ function initMap(){
 	// if been defined from another article
 	var bounds = new google.maps.LatLngBounds();
 	if(articleProp !== null){
+		console.log('articleprop');
 		console.log(articleProp);
 		bounds.extend(articleProp[1]);
 		bounds.extend(articleProp[3]);
